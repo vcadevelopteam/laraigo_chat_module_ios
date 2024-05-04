dedd73dcbcc8c7795087549a6c60e8958f839d7b
JCasanovaVCA <jorge.casanova@vcaperu.com>
2024-05-03 20:04:39 -0500

    feat: last changes 1.0.5+8

--- iOSChatSocket.swift
+++ iOSChatSocket.swift
@@ -1,73 +1,48 @@
-// * Flutter Module implementation on iOS Project with (StoryBoard implementation() 
-import UIKit
+
+import SwiftUI
 import Flutter
-import FlutterPluginRegistrant
 
-class ViewController: UIViewController {
-    let laraigoChatSocket = LaraigoChatSocket()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Create and configure the button
-        let launchButton = UIButton(type: .system)
-        launchButton.setTitle("Launch Chat", for: .normal)
-        launchButton.addTarget(self, action: #selector(launchChat), for: .touchUpInside)
-
-        // Set up button constraints (adjust as needed)
-        launchButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(launchButton)
-        NSLayoutConstraint.activate([
-            launchButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            launchButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-        ])
-    }
-
-    @objc func launchChat() {
-        // Call the method to initialize the chat socket
-        laraigoChatSocket.initChatSocket(integrationId: "integrationId", viewController: self)
-    }
-}
-
-class LaraigoChatSocket {
-
-    private var flutterEngine: FlutterEngine = FlutterEngine(name: "flutter_engine")
-    private let channelName = "laraigo_chat_communication_channel"
-
-    func initChatSocket(integrationId: String, viewController: UIViewController) {
-        setupFlutterEngine()
-        setupMethodChannel(integrationId)
-        launchFlutterModule(viewController: viewController)
-    }
-
-    private func setupFlutterEngine() {
-        flutterEngine.run()
-    }
-
-    private func setupMethodChannel(_ integrationId: String) {
-        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
-        let mapWithValues: [String: Any] = ["integrationId": integrationId]
-
-        let channel = FlutterMethodChannel(
-            name: channelName,
-            binaryMessenger: flutterViewController.binaryMessenger
-        )
+struct HomeView: View {
+    @EnvironmentObject var flutterDependencies: FlutterDependencies
+    @State var flutterMethodChannel : FlutterMethodChannel?
+    
+    var body: some View {
         
-        channel.setMethodCallHandler { (call, result) in
-            if call.method == "testingSendData" {
-                print("Sending map data to Flutter")
-                result.self(mapWithValues)
+        VStack {
+            Button(action: {showFlutter()}) {
+                            Text("Open Flutter View")
+                                .font(.headline)
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }.padding(.vertical, 10)
             }
         }
+    func showFlutter(){
+        guard
+            let windowScene = UIApplication.shared.connectedScenes
+                .first(where: { $0.activationState == .foregroundActive && $0 is UIWindowScene }) as? UIWindowScene,
+            let window = windowScene.windows.first(where: \.isKeyWindow),
+            let rootViewController = window.rootViewController
+        else { return }
         
-    }
-
-    private func launchFlutterModule(viewController: UIViewController) {
-        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
-        
-        viewController.present(flutterViewController, animated: true, completion: nil)
+        flutterMethodChannel = FlutterMethodChannel(name: "laraigo_chat_communication_channel", binaryMessenger: flutterDependencies.flutterEngine.binaryMessenger)
+    
+        flutterMethodChannel?.invokeMethod("testingSendData", arguments: ["integrationId":"6567ade24933f425469910e1"])
+             
+        // Create a FlutterViewController from pre-warm FlutterEngine
+        let flutterViewController = FlutterViewController(
+                 engine: flutterDependencies.flutterEngine,
+                 nibName: nil,
+                 bundle: nil)
+        flutterViewController.modalPresentationStyle = .overCurrentContext
+        flutterViewController.isViewOpaque = false
+        rootViewController.present(flutterViewController, animated: true)
     }
 }
 
 
-// * Flutter Module implementation on iOS Project with (SwiftUI implementation() 
+#Preview {
+    HomeView()
+}
